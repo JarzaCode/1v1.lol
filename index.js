162a9a5672f14f8bd8b63584a234a8bbc8512659
JarzaCode <isaiah.withers@students.coderva.org>
2024-12-17 23:00:05 +0000

    Consistent styling

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const electron = require('electron')
+const electron = require('electron');
 
 let window;
-function create(){
-window = new electron.BrowserWindow({
+function create() {
+  window = new electron.BrowserWindow({
     width: 1080,
     minWidth: 680,
     height: 840,
@@ -15,14 +15,13 @@ window = new electron.BrowserWindow({
     "autoHideMenuBar": true,
     "backgroundColor": "black",
     "fullscreen": false,
-    "icon": "download.png"
-})
-//window.setIcon(__dirname + "/images.png")
-window.loadURL("https://1v1.lol")
-//window.setFullScreen(true)
-window.setClosable(true)
-
-}
+    "icon": "download.png",
+  });
+  //window.setIcon(__dirname + "/images.png");
+  window.loadURL("https://1v1.lol");
+  //window.setFullScreen(true);
+  window.setClosable(true);
+};
 electron.app.on("ready" , () =>{
-create()
-})
+  create();
+});
